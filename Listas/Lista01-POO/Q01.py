0967a3194a7b81d258145f6f1e61c55bfd06f168
Lielly <93990842+Lielly@users.noreply.github.com>
2026-03-24 09:46:04 -0300

    Adicionando questão 1 da Lista 1 de POO em python

---
+++ Listas/Lista01-POO/Q01.py
@@ -0,0 +1,11 @@
+class Circulo:
+    def __init__(self):
+        self.raio = 0
+    def calc_area(self):
+        return 3.14 * (self.raio**2)
+    def calc_circunferencia(self):
+        return 2 * 3.14 * self.raio
+x = Circulo()
+x.raio = 3
+print(x.calc_area())
+print(x.calc_circunferencia())
