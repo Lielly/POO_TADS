e2fe6bd284a1ca4d36b5815d6c733efb3ebb125c
Lielly <93990842+Lielly@users.noreply.github.com>
2026-03-30 18:51:41 +0000

    Adicionando Q3 e Q4 da lista 01 de POO incompletas

---
+++ Listas/Lista01-POO/Q04.py
@@ -0,0 +1,11 @@
+class Entrada_Cinema:
+    def __init__(self):
+        self.dia = ""
+        self.horario = 0
+    def calc_entrada_inteira(self):
+        if self.dia == segunda
+    def calc_meiaentrada(self):
+        
+
+print(f"A entrada inteira na {dia} às {horario} é R$")
+print(f"A meia entrada na {dia} às {horario} é R$")
